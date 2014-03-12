d68214f933a07b7eb06efe9a68e89c977ec766fd
Bruno Aquino <theodorodeaquino@gmail.com>
2014-03-11 23:54:03 -0300

    Commit com a classe DAO Usuário

---
+++ src/main/java/br/com/dk/pjweb2/dao/UsuarioDAO.java
@@ -0,0 +1,43 @@
+package br.com.dk.pjweb2.dao;
+
+import javax.persistence.EntityManager;
+
+import br.com.dk.pjweb2.entidade.Usuario;
+import br.com.dk.pjweb2.util.JPAUtil;
+
+public class UsuarioDAO {
+
+	EntityManager em;
+
+	public UsuarioDAO(EntityManager em) {
+		super();
+		this.em = JPAUtil.getEntityManager();
+	}
+
+	public void salvar(Usuario usu) {
+
+		try {
+			em.getTransaction().begin();
+			em.persist(usu);
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			em.getTransaction().rollback();
+		}
+
+		em.close();
+
+	}
+	
+	public void alterar(Usuario usu){
+		try{
+			em.getTransaction().begin();
+			em.persist(em.merge(usu));
+			em.getTransaction().commit();
+		}catch(Exception e){
+			em.getTransaction().rollback();
+		}
+		
+		em.close();
+	}
+
+}
